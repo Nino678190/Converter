6ea7dadd477b51ce1295f26b8a6d69bb7356981b
Nino678190 <115342129+Nino678190@users.noreply.github.com>
2024-07-24 19:49:43 +0200

    USD & ISK hinzugefügt

--- converter.py
+++ converter.py
@@ -1,6 +1,8 @@
-nok = 0.085
-sek = 0.084
-dkk = 0.12
+nok = 0.084
+sek = 0.086
+dkk = 0.13
+usd = 0.92
+isk = 0.0067
 #eingabe = 100
 
 def frage():
@@ -46,7 +48,30 @@ def EurToNok(eingabe):
     print(output)
     frage()
 
-#NokToEur(eingabe)
+def UsdToEur(eingabe):
+    output = eingabe * usd
+    output = round(output,2)
+    print(output)
+    frage()
+
+def EurToUsd(eingabe):
+    output = (1/usd)*eingabe
+    output = round(output,2)
+    print(output)
+    frage()
+
+def IskToEur(eingabe):
+    output = eingabe * isk
+    output = round(output,2)
+    print(output)
+    frage()
+
+def EurToIsk(eingabe):
+    output = (1/isk)*eingabe
+    output = round(output,2)
+    print(output)
+    frage()
+ #NokToEur(eingabe)
 
 def starten():
     print("1: NOK to Euro")
@@ -55,8 +80,17 @@ def starten():
     print("4: Euro to DKK")
     print("5: Euro to SEK")
     print("6: Euro to NOK")
+    print("7: USD to Euro")
+    print("8: Euro to USD")
+    print("9: ISK to Euro")
+    print("10: Euro to ISK")
     auswahl = input("Was soll umgerechnet werden?")
-    eingabe = int(input("Welche Menge?"))
+    eingabe = float(input("Welche Menge?"))
+    try:
+        val = float(eingabe)
+    except ValueError:
+        print("Das ist keine Zahl")
+        start()
     if auswahl == '1':
         NokToEur(eingabe)
     elif auswahl == '2':
@@ -69,6 +103,14 @@ def starten():
         EurToSek(eingabe)
     elif auswahl == '6':
         EurToNok(eingabe)
+    elif auswahl == '7':
+        UsdToEur(eingabe)
+    elif auswahl == '8':
+        EurToUsd(eingabe)
+    elif auswahl == '9':
+        IskToEur(eingabe)
+    elif auswahl == '10':
+        EurToIsk(eingabe)
     else:
         print("ungueltige Eingabe")
         starten()
